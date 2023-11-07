e189987c51aae82d656b4db766a95ed71e21ef9a
JeffreyDinackus <jeffreydinackus@proton.me>
2023-11-07 13:05:47 -0500

    just going through textbook

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ const exponentialNumbers = numbers.map(function (number) {
 console.log(exponentialNumbers);
 // [1, 4, 9, 16]
 
+const AddOne = (x) => x + 1;
+
+
 const list = [
   {
     title: 'React',
@@ -33,26 +36,49 @@ const list = [
     points: 5,
     objectID: 3,
   }]
-function App() {
+
+const List = () => {
   return (
-    <div>
-      ...
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <ul>
+      {list.map((item) => {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
+
+const App = () => {
+  return (
+    < >
+      <h1>Hacker Stories</h1>
+      <AddOne />
+      <Search />
+      <List />
+      <List />
+    </ >)
+}
+
+
+
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    Fundamentals of React
+    40
+    <input id="search" type="text" />
+  </div>
+);
+
+
+console.log(App())
 
 export default App;
