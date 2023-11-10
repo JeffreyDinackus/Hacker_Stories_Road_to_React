c4e9074a86bdd99f7c12f281fa2a649dfed463c6
JeffreyDinackus <jeffreydinackus@proton.me>
2023-11-10 12:18:18 -0500

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 // const title = 'React';
+/* eslint-disable */ // TODO: upgrade to latest eslint tooling
 
 const numbers = [1, 2, 3, 4];
 const exponentialNumbers = numbers.map(function (number) {
@@ -37,59 +38,66 @@ function Counter() {
 }
 
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'shadcnui',
-    url: 'xd',
-    author: 'shad',
-    num_comments: 2,
-    points: 5,
-    objectID: 3,
-  }]
 
-const List = () => {
-  return (
-    <ul>
-      {list.map((item) => {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
 
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Welcome = ({ text }) => {
+  return <h1 style={{ color: 'red' }}>{text.greeting}</h1>;
+};
+
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'shadcnui',
+      url: 'https://ui.shadcn.com/',
+      author: 'shad',
+      num_comments: 2,
+      points: 5,
+      objectID: 3,
+    }]
+  const greetingObject = { greeting: 'Welcome to React' };
+
   return (
     < >
       <h1>Hacker Stories</h1>
       <AddOne />
       <Search />
-      <List />
-      <List />
+      <Welcome text={greetingObject} />
+      <List list={stories} />
       <Counter />
     </ >)
 }
